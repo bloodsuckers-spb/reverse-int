6786c9c153e3497cde86691317a8dfb2d26129ea
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2022-12-29 02:33:06 +0300

    feat: implement reverse-int func with RegExp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = reverse = (x) =>
+    `${(x ^ (x >> 31)) - (x >> 31)}`
+    .replace(/\d/g, (_, i, str) => str[str.length - 1 - i]) >> 0;
+
+    /* ещё один из вариантов решения
+    const reverse = (x) => 
+        `${x}`
+            .replace("-", "")
+            .replace(/\d/g, (_, i, str) => str[str.length - 1 - i]) >> 0; 
+    */
