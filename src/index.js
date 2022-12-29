729d892e05b8b7bc8a8b0b01453c0386b0010ad5
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2022-12-29 18:24:52 +0300

    feat: add another algorithm

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ module.exports = reverse = (x) =>
     `${(x ^ (x >> 31)) - (x >> 31)}`
     .replace(/\d/g, (_, i, str) => str[str.length - 1 - i]) >> 0;
 
-    /* ещё один из вариантов решения
-    const reverse = (x) => 
+    /*
+        Более эффектиный алгоритм:
+
+        const reverse = (num) => {
+            let result = 0;
+            num = (num ^ (num >> 31)) - (num >> 31);
+            while (num) {
+                result = result * 10 + (num % 10);
+                num = (num / 10) | 0;
+            }
+            return result;
+        };
+    */
+
+    /* 
+        Ёщё один из вариантов решения c помощью регулярок:
+    
+        const reverse = (x) => 
         `${x}`
             .replace("-", "")
             .replace(/\d/g, (_, i, str) => str[str.length - 1 - i]) >> 0; 
